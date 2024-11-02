8713df71d0d21eb8822fe0dad81ce6299a065f08
Martin Gronek <martin@gronek.de>
2024-11-02 18:37:26 +0100

    about final

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -94,9 +94,7 @@
 
 
 		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
-			{#if !$WEBUI_NAME.includes('Open WebUI')}
-				<span class=" text-gray-500 dark:text-gray-300 font-medium">{'7AI One GPT'}</span> -
-			{/if}
+			<span class="text-gray-500 dark:text-gray-300 font-medium">{$WEBUI_NAME}</span>
 			Operated by 
 			<a
 				class=" text-gray-500 dark:text-gray-300 font-medium"
